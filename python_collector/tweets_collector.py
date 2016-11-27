bce113fb0de947593a18e2b89e225d19a1e399dc
nicor88 <ncorda@babbel.com>
2016-11-27 20:08:34 +0100

    added tweepy

--- python_collector/tweets_collector.py
+++ python_collector/tweets_collector.py
@@ -5,14 +5,19 @@ Examples
 --------
 # Example
 >>> host = 'localhost:4200'
->>> collector = TweetsCollector(host=host)
+>>> keywords = ['referendum']
+>>> collector = TweetsCollector.collect_and_save(host=host, keywords=keywords)
 
 """
 
 import logging
 import os
 import yaml
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 import python_collector.settings
+
 from crate import client
 
 logging.basicConfig(level=logging.INFO)
@@ -20,25 +25,34 @@ logger = logging.getLogger(__name__)
 
 
 class TweetsCollector:
-    def __init__(self, *, host):
+    def __init__(self, *, host, keywords):
         self.crate_host = host
+        self.keywords = keywords
+        configs = self.configs_loader()
+        twitter_auth = configs['twitter']
+
+        # set twitter auth
+        self.auth = OAuthHandler(twitter_auth['consumer_key'], twitter_auth['consumer_secret'])
+        self.auth.set_access_token(twitter_auth['access_token'], twitter_auth['access_secret'])
 
         # connect to crate
         try:
-            connection = client.connect(self.crate_host)
+            self.crate = client.connect(self.crate_host)
         except:
             logger.info('Error connecting to ' + self.crate_host)
 
     @classmethod
-    def collect_and_save(cls, *, host):
-        c = cls(host=host)
+    def collect_and_save(cls, *, host, keywords):
+        c = cls(host=host, keywords=keywords)
         return c.get_tweets()
 
     def get_tweets(self):
+        twitter_stream = Stream(self.auth, PersistTweets(crate=self.crate))
+        twitter_stream.filter(track=self.keywords)
         return self
 
     @staticmethod
-    def config_loader():
+    def configs_loader():
         settings_path = os.path.realpath(os.path.dirname(python_collector.settings.__file__))
         configs_file = os.path.join(settings_path, 'configs.yml')
         try:
@@ -47,3 +61,17 @@ class TweetsCollector:
                 return configs
         except IOError:
             logger.debug('No configs.yml found')
+
+
+class PersistTweets(StreamListener):
+    def __init__(self, *, crate):
+        super(StreamListener, self).__init__()
+        self.crate = crate
+
+    def on_data(self, data):
+        # todo here persist data on crate
+        print(data)
+
+    def on_error(self, status):
+        print(status)
+        return True
