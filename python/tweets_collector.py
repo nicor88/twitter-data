a8d00134b1387bc9c19870026ca2a7cecef5b3bf
nicor88 <ncorda@babbel.com>
2016-11-27 18:36:13 +0100

    added scheleton class

--- python/tweets_collector.py
+++ python/tweets_collector.py
@@ -1,3 +1,8 @@
+"""
+Class to collect tweets with specific keywords and save to crateDb
+
+"""
+
 import logging
 from crate import client
 
@@ -6,4 +11,21 @@ logger = logging.getLogger(__name__)
 
 CRATE_HOST = 'localhost:4200'
 
-connection = client.connect(CRATE_HOST)
+
+class TweetsCollector:
+    def __init__(self, *, host):
+        self.crate_host = host
+
+        # connect to crate
+        try:
+            connection = client.connect(self.crate_host)
+        except:
+            logger.info('Error connecting to ' + self.crate_host)
+
+    @classmethod
+    def collect_and_save(cls, *, host):
+        c = cls(host=host)
+        return c.get_tweets()
+
+    def get_tweets(self):
+        return self
