544a019ef8dc76dc70bb302fb13fd9a7b922996d
nicor88 <ncorda@babbel.com>
2016-11-27 21:14:27 +0100

    added table creation

--- python_collector/tweets_collector.py
+++ python_collector/tweets_collector.py
@@ -37,7 +37,8 @@ class TweetsCollector:
 
         # connect to crate
         try:
-            self.crate = client.connect(self.crate_host)
+            crate_connection = client.connect(self.crate_host)
+            self.crate_cursor = crate_connection.cursor()
         except:
             logger.info('Error connecting to ' + self.crate_host)
 
@@ -47,9 +48,16 @@ class TweetsCollector:
         return c.get_tweets()
 
     def get_tweets(self):
-        twitter_stream = Stream(self.auth, PersistTweets(crate=self.crate))
+        twitter_stream = Stream(self.auth, PersistTweets(crate_cursor=self.crate_cursor))
         twitter_stream.filter(track=self.keywords)
         return self
+
+    def create_tweets_table(self):
+        table_statement = self.tweets_table()
+        try:
+            self.crate_cursor.execute(table_statement)
+        except:
+            logger.error('Impossible to create the table')
 
     @staticmethod
     def configs_loader():
@@ -62,11 +70,32 @@ class TweetsCollector:
         except IOError:
             logger.debug('No configs.yml found')
 
+    @staticmethod
+    def tweets_table():
+        return """create table tweets_test_python (
+                    id string,
+                    created_at timestamp,
+                    retweeted boolean,
+                    source string,
+                    sentiment_tweet_score float,
+                    text string index using fulltext with (analyzer = "standard"),
+                    user object(dynamic) as (
+                        id string,
+                        created_at timestamp,
+                        description string,
+                        location string,
+                        followers_count integer,
+                        statuses_count integer,
+                        friends_count integer,
+                        verified boolean
+                    )
+                )"""
+
 
 class PersistTweets(StreamListener):
-    def __init__(self, *, crate):
+    def __init__(self, *, crate_cursor):
         super(StreamListener, self).__init__()
-        self.crate = crate
+        self.crate_cursor = crate_cursor
 
     def on_data(self, data):
         # todo here persist data on crate
@@ -75,3 +104,6 @@ class PersistTweets(StreamListener):
     def on_error(self, status):
         print(status)
         return True
+
+# to stop the listener try
+# PersistTweets.running = False
